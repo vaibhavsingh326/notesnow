eb17ae8216d1f9154ffd37fa76111c7783c58030
Vaibhav Singh <vaibhav.singh326@gmail.com>
2024-02-14 12:53:22 +0530

    new backend

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const connection = require("./db/connection.js");
 const {noteRouter} = require("./routes/note.routes.js");
 const userRouter = require("./routes/user.routes.js");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
 
